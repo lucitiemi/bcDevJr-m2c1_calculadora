7f89e445d621137a6e41dd61d7b643734c3267df
Luciana <lt.shimoyama@gmail.com>
2024-10-29 22:39:21 -0300

    closes #4 - Implementação das 4 operações

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-function calc() {
+function calc(op) {
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
+    var output = 0;
 
-    var output = num1 + num2;
-
-    document.getElementById("output").value = output;
+    switch(op) {
+        case 'a': var result = num1 + num2; break;
+        case 's': var result = num1 - num2; break;
+        case 'm': var result = num1 * num2; break;
+        case 'd': var result = num1 / num2; break;
+    }
+   
+    document.getElementById("output").value = result;
 }
